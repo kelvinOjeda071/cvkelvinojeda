bab9689bf9bbb5928d1dd9e9e8262ccf3df7e78f
kelvinOjeda071 <kelvin.ojeda.2020b@gmail.com>
2022-07-31 18:57:58 -0500

    Open in a new windows all the reference in the page

---
+++ script/main.js
@@ -0,0 +1,23 @@
+function myPhone() {
+    window.open("https://wa.me/+593999790960?text=Hola%20Kelvin!");
+}
+
+function myInst() {
+    window.open("https://www.instagram.com/kevinojeda01/");
+}
+
+function myFace() {
+    window.open("https://www.facebook.com/profile.php?id=100072561631108");
+}
+
+function myGit() {
+    window.open("https://github.com/kelvinOjeda071?tab=repositories");
+}
+
+function myEnglishLevel() {
+    window.open("https://epnecuador-my.sharepoint.com/:b:/g/personal/kelvin_ojeda_epn_edu_ec/EbIpiqHvot5AmUFY-wFstIwB2S4ak14u7nB7fvJc-_voQA?e=tNEWZt");
+}
+
+function printCv() {
+    window.open("https://kelvinojeda071.github.io/cvkelvinojeda/docs/CVKelvinOjedaQuiroz.pdf");
+}
